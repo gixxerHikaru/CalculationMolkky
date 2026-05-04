7484d65b9944ace92481f937a0374835822aede2
gixxerHikaru <yuki.sakurai0617@gmail.com>
2026-05-04 09:52:36 +0900

    テストリファクタリング
    次のターンとかはもういいかなって思ったので、ヘルパーから削除(ちゃんと待ちたい時はその都度assertするため)
    上記が意外とテストの同期・非同期の処理を複雑にしていたので削除

--- react-router-7-prisma/__test__/routes/home.spec.tsx
+++ react-router-7-prisma/__test__/routes/home.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, screen, waitFor, within } from '@testing-library/react';
 import { assert, expect, test } from 'vitest';
 import Home from '../../app/routes/home';
 import { createRoutesStub } from 'react-router';
@@ -65,91 +65,82 @@ describe('スコアの更新', () => {
   async function assertTeamScore(team: 'A' | 'B', expectedScore: number) {
     const teamBox = await screen.findByTestId(team === 'A' ? 'team-a-box' : 'team-b-box');
 
-    expect(teamBox).toHaveTextContent(`${expectedScore}点`);
+    const scoreElement = await within(teamBox).findByText(new RegExp(`${expectedScore}点`));
+
+    assert.exists(scoreElement, `チーム${team}のスコアが${expectedScore}点であること`);
   }
 
-  async function assertState(expectedA: number, expectedB: number, expectedTurn?: string) {
+  async function assertState(expectedA: number, expectedB: number) {
     await assertTeamScore('A', expectedA);
     await assertTeamScore('B', expectedB);
-
-    if (expectedTurn) {
-      expect(await screen.findByText(expectedTurn)).toBeInTheDocument();
-    }
   }
 
   async function assertTeamState(team: 'A' | 'B', expectedScore: number, expectedFoul: number) {
     const teamId = team === 'A' ? 'team-a-box' : 'team-b-box';
     const teamBox = await screen.findByTestId(teamId);
 
-    expect(within(teamBox).getByText(`${expectedScore}点`)).toBeInTheDocument();
+    const scoreEl = await within(teamBox).findByText(new RegExp(`${expectedScore}点`));
+    assert.exists(scoreEl);
 
     if (expectedFoul > 0) {
-      expect(within(teamBox).getByText(`ファウル：${expectedFoul}`)).toBeInTheDocument();
+      const foulEl = await within(teamBox).findByText(new RegExp(`ファウル：${expectedFoul}`));
+      assert.exists(foulEl);
     } else {
-      expect(within(teamBox).queryByText(/ファウル：/)).not.toBeInTheDocument();
+      assert.isNull(within(teamBox).queryByText(/ファウル：/));
     }
   }
 
   const user = userEvent.setup();
 
-  async function playAndAssert(
-    point: number,
-    expectedA: number,
-    expectedB: number,
-    expectedTurn: string
-  ) {
+  async function playAndAssert(point: number, expectedA: number, expectedB: number) {
     const button = await screen.findByRole('button', { name: `${point}点` });
     await user.click(button);
-    assertState(expectedA, expectedB, expectedTurn);
+    await assertState(expectedA, expectedB);
   }
 
-  async function clickBackAndAssert(expectedA: number, expectedB: number, expectedTurn: string) {
+  async function clickBackAndAssert(expectedA: number, expectedB: number) {
     const button = await screen.findByRole('button', { name: '戻る' });
     await user.click(button);
-    assertState(expectedA, expectedB);
+    await assertState(expectedA, expectedB);
   }
 
   test('チームAとチームBが交互に得点し、スコアと攻撃権が正しく遷移する', async () => {
     render(<Stub initialEntries={['/calculation_molkky']} />);
 
     const steps = [
-      { point: 12, a: 12, b: 0, nextTurn: '🟦チームBの番です' },
-      { point: 11, a: 12, b: 11, nextTurn: '🟥チームAの番です' },
-      { point: 10, a: 22, b: 11, nextTurn: '🟦チームBの番です' },
-      { point: 9, a: 22, b: 20, nextTurn: '🟥チームAの番です' },
-      { point: 8, a: 30, b: 20, nextTurn: '🟦チームBの番です' },
-      { point: 7, a: 30, b: 27, nextTurn: '🟥チームAの番です' },
-      { point: 6, a: 36, b: 27, nextTurn: '🟦チームBの番です' },
-      { point: 5, a: 36, b: 32, nextTurn: '🟥チームAの番です' },
-      { point: 4, a: 40, b: 32, nextTurn: '🟦チームBの番です' },
-      { point: 3, a: 40, b: 35, nextTurn: '🟥チームAの番です' },
-      { point: 2, a: 42, b: 35, nextTurn: '🟦チームBの番です' },
-      { point: 1, a: 42, b: 36, nextTurn: '🟥チームAの番です' },
+      { point: 12, a: 12, b: 0 },
+      { point: 11, a: 12, b: 11 },
+      { point: 10, a: 22, b: 11 },
+      { point: 9, a: 22, b: 20 },
+      { point: 8, a: 30, b: 20 },
+      { point: 7, a: 30, b: 27 },
+      { point: 6, a: 36, b: 27 },
+      { point: 5, a: 36, b: 32 },
+      { point: 4, a: 40, b: 32 },
+      { point: 3, a: 40, b: 35 },
+      { point: 2, a: 42, b: 35 },
+      { point: 1, a: 42, b: 36 },
     ];
 
     for (const step of steps) {
-      await playAndAssert(step.point, step.a, step.b, step.nextTurn);
+      await playAndAssert(step.point, step.a, step.b);
     }
   });
 
   test('番手のチームの合計スコアにハイライトが当たり、そうでないチームにはつかない', async () => {
     render(<Stub initialEntries={['/calculation_molkky']} />);
-    let teamABox = await screen.findByTestId('team-a-box');
-    let teamBBox = await screen.findByTestId('team-b-box');
-    expect(teamABox).toHaveClass('bg-yellow-100 dark:bg-yellow-900/20');
-    expect(teamBBox).not.toHaveClass('bg-yellow-100 dark:bg-yellow-900/20');
 
-    await playAndAssert(5, 5, 0, '🟦チームBの番です');
-    teamABox = await screen.findByTestId('team-a-box');
-    teamBBox = await screen.findByTestId('team-b-box');
-    expect(teamABox).not.toHaveClass('bg-yellow-100 dark:bg-yellow-900/20');
-    expect(teamBBox).toHaveClass('bg-yellow-100 dark:bg-yellow-900/20');
+    await waitFor(async () => {
+      const teamABox = await screen.findByTestId('team-a-box');
+      expect(teamABox).toHaveClass('bg-yellow-100');
+    });
 
-    await clickBackAndAssert(5, 0, '🟦チームBの番です');
-    teamABox = await screen.findByTestId('team-a-box');
-    teamBBox = await screen.findByTestId('team-b-box');
-    expect(teamABox).toHaveClass('bg-yellow-100 dark:bg-yellow-900/20');
-    expect(teamBBox).not.toHaveClass('bg-yellow-100 dark:bg-yellow-900/20');
+    await playAndAssert(5, 5, 0);
+
+    await waitFor(async () => {
+      const teamBBox = await screen.findByTestId('team-b-box');
+      expect(teamBBox).toHaveClass('bg-yellow-100');
+    });
   });
 
   test('ファウルボタンを押すと、得点は加算されずに攻撃権が移り、合計スコアの下にファウル回数が表示される', async () => {
@@ -168,26 +159,29 @@ describe('スコアの更新', () => {
     render(<Stub initialEntries={['/calculation_molkky']} />);
 
     const foulButton = await screen.findByRole('button', { name: 'ファウル' });
-    await user.click(foulButton);
 
+    await user.click(foulButton);
     await assertTeamState('A', 0, 1);
 
-    assert.isOk(screen.getByText('🟦チームBの番です'), 'チームBの番が表示されていること');
+    assert.isOk(await screen.findByText('🟦チームBの番です'));
     await user.click(foulButton);
+
     await assertTeamState('A', 0, 1);
     await assertTeamState('B', 0, 1);
 
-    await playAndAssert(5, 0, 5, '🟥チームAの番です');
+    assert.isOk(await screen.findByText('🟥チームAの番です'));
+    await playAndAssert(5, 5, 0);
+
     await assertTeamState('A', 5, 0);
     await assertTeamState('B', 0, 1);
   });
 
   test('戻るボタンを押すと、前の状態に戻る', async () => {
     render(<Stub initialEntries={['/calculation_molkky']} />);
-    await playAndAssert(5, 5, 0, '🟦チームBの番です');
-    await playAndAssert(3, 5, 3, '🟥チームAの番です');
+    await playAndAssert(5, 5, 0);
+    await playAndAssert(3, 5, 3);
 
-    await clickBackAndAssert(5, 0, '🟦チームBの番です');
-    await clickBackAndAssert(0, 0, '🟥チームAの番です');
+    await clickBackAndAssert(5, 0);
+    await clickBackAndAssert(0, 0);
   });
 });
