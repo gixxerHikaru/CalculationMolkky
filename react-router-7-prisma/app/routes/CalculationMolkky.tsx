6992fb55e5ca8c6ad2669828eb8fed08ec13925c
gixxerHikaru <yuki.sakurai0617@gmail.com>
2026-05-05 23:30:50 +0900

    メニュー画面の作成、2チーム用の移管と修正
    動作確認してたら、モーダルのダークモードの時どっちが買ったかわからない問題が発生したのでこのあと修正

--- react-router-7-prisma/app/routes/CalculationMolkky.tsx
+++ react-router-7-prisma/app/routes/CalculationMolkky.tsx
@@ -12,7 +12,7 @@ type State = {
 
 const initialState: State = { scoreA: 0, foulA: 0, scoreB: 0, foulB: 0, turn: 'A' };
 
-export function CalculationMolkky() {
+export default function CalculationMolkky() {
   const [winner, setWinner] = useState<Team | null>(null);
   const [loser, setLoser] = useState<Team | null>(null);
   const [history, setHistory] = useState<State[]>([initialState]);
@@ -62,7 +62,7 @@ export function CalculationMolkky() {
     <main className="min-h-screen flex flex-col items-center pt-8 pb-4 px-4 bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
       <div className="w-full max-w-md flex flex-col items-center gap-4 text-gray-800 dark:text-gray-100">
         <header className="w-full flex flex-col items-center py-2 bg-white dark:bg-gray-800 rounded-l shadow-sm border border-gray-100 dark:border-gray-700">
-          <h1 className="text-xl font-bold">モルック・スコア計算</h1>
+          <h1 className="text-xl font-bold">モルック・スコア計算(2チーム)</h1>
         </header>
 
         <div className="w-full flex items-stretch bg-white dark:bg-gray-800 rounded-xl shadow-md border border-gray-100 dark:border-gray-700 overflow-hidden">
--- react-router-7-prisma/app/routes/home.tsx
+++ react-router-7-prisma/app/routes/home.tsx
@@ -1,5 +1,5 @@
+import { Link } from 'react-router';
 import type { Route } from './+types/home';
-import { CalculationMolkky } from './CalculationMolkky';
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <CalculationMolkky />;
+  return (
+    <>
+      <main className="min-h-screen flex flex-col items-center pt-8 pb-4 px-4 bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
+        <div className="w-full max-w-md flex flex-col items-center gap-4 text-gray-800 dark:text-gray-100">
+          <header className="w-full flex flex-col items-center py-2 bg-white dark:bg-gray-800 rounded-l shadow-sm border border-gray-100 dark:border-gray-700">
+            <h1 className="text-xl font-bold">モルック・スコア計算</h1>
+          </header>
+
+          <Link to="two_team">2チームで遊ぶ</Link>
+        </div>
+      </main>
+    </>
+  );
 }
