3c09f3447a2cdb5c8df9764c626375bae639f311
gixxerHikaru <168717231+gixxerHikaru@users.noreply.github.com>
2026-05-10 11:33:14 +0900

    Revert "Add Cloudflare Workers configuration"

--- react-router-7-prisma/react-router.config.ts
+++ react-router-7-prisma/react-router.config.ts
@@ -4,8 +4,4 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-
-  future: {
-    unstable_viteEnvironmentApi: true
-  }
 } satisfies Config;
